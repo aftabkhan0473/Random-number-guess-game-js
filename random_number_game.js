d09ccc66a6911a42d98a65d0c6c0d3166588a948
MD AFTAB ALI <98120110+aftabkhan0473@users.noreply.github.com>
2022-12-20 00:04:05 +0530

    random number guess game done

---
+++ random_number_game.js
@@ -0,0 +1,11 @@
+// Math.random() prints random number between 0 and 1.
+// let random = Math.random()*100;
+let computer = Math.round(Math.random()*100);
+console.log(computer);
+
+let user = Number.parseInt(prompt("Enter number between 0 to 100: "));
+console.log(user);
+
+if(computer>user){
+
+}
